f6052639d38aa2d1e7a2dfdac18af68145674c6b
Lucas Santos <lucas.prog@outlook.com>
2022-04-12 17:31:29 -0300

    analise do projeto

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import br.com.boot.camp.dominio.Curso;
+import br.com.boot.camp.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Java script");
+        curso2.setDescricao("descrição curso java script");
+        curso2.setCargaHoraria(4);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
